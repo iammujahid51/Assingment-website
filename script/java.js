13668ceadee04d0093ab7f2ada3686a753b9ddb5
iammujahid51 <mujahidhossain5980@gmail.com>
2025-03-15 19:42:32 +0600

    added back file

---
+++ script/java.js
@@ -0,0 +1,229 @@
+
+
+function disableButton() {
+  alert("Button clicked!");
+  const btn = document.getElementById('btn-completed-1');
+  btn.disabled = true;
+  btn.classList.add('bg-gray-400'); // Optional: You can change the background to gray to show it's disabled
+  btn.classList.remove('bg-blue-800'); // Remove the active button color
+ 
+
+  const countElement = document.getElementById('btn-complet');
+  let currentCount = parseInt(countElement.innerText); // Get the current number
+  if (currentCount > 0) {
+    countElement.innerText = currentCount + 1; // Decrease by 1
+  }
+
+  // Decrease the number in <h5> by 1
+  const secondCountElement = document.getElementById('sceund-number');
+  
+  
+  let currentSecondCount = parseInt(secondCountElement.innerText); // Get the current number
+  if (currentSecondCount > 0) {
+    secondCountElement.innerText = currentSecondCount - 1; // Decrease by 1
+  }
+  const historyElement = document.getElementById('history');
+  const historyMessage = `clar fhgetfg7uc iedfsyreigdgf erdfy8urfewer dfsy8sfg`;
+  historyElement.innerHTML = historyMessage;
+
+ document.getElementById('history-1').style.backgroundColor = 'white'
+
+
+}
+
+
+
+
+
+
+
+
+
+function disableButtonn() {
+  alert("Button clicked!");
+  const btn = document.getElementById('btn-completed-2');
+  btn.disabled = true;
+  btn.classList.add('bg-gray-400'); // Optional: You can change the background to gray to show it's disabled
+  btn.classList.remove('bg-blue-800'); // Remove the active button color
+ 
+
+  const countElement = document.getElementById('btn-complet');
+  let currentCount = parseInt(countElement.innerText); // Get the current number
+  if (currentCount > 0) {
+    countElement.innerText = currentCount + 1; // Decrease by 1
+  }
+
+  // Decrease the number in <h5> by 1
+  const secondCountElement = document.getElementById('sceund-number');
+  
+  
+  let currentSecondCount = parseInt(secondCountElement.innerText); // Get the current number
+  if (currentSecondCount > 0) {
+    secondCountElement.innerText = currentSecondCount - 1; // Decrease by 1
+  }
+  const historyElemen = document.getElementById('history');
+  const historyMessage = `bsbyd`;
+  historyElemen.innerHTML = historyMessage;
+
+   document.getElementById('history').style.backgroundColor = 'white'
+
+
+}
+
+
+
+
+function disableButto() {
+  alert("Button clicked!");
+  const btn = document.getElementById('btn-completed-3');
+  btn.disabled = true;
+  btn.classList.add('bg-gray-400'); // Optional: You can change the background to gray to show it's disabled
+  btn.classList.remove('bg-blue-800'); // Remove the active button color
+ 
+
+  const countElement = document.getElementById('btn-complet');
+  let currentCount = parseInt(countElement.innerText); // Get the current number
+  if (currentCount > 0) {
+    countElement.innerText = currentCount + 1; // Decrease by 1
+  }
+
+  // Decrease the number in <h5> by 1
+  const secondCountElement = document.getElementById('sceund-number');
+  
+  
+  let currentSecondCount = parseInt(secondCountElement.innerText); // Get the current number
+  if (currentSecondCount > 0) {
+    secondCountElement.innerText = currentSecondCount - 1; // Decrease by 1
+  }
+  const historyElement = document.getElementById('history');
+  const historyMessage = `bsbyd`;
+  historyElement.innerHTML = historyMessage;
+
+   document.getElementById('history').style.backgroundColor = 'white'
+
+
+}
+
+
+
+
+function disablebutton() {
+  alert("Button clicked!");
+  const btn = document.getElementById('btn-completed-4');
+  btn.disabled = true;
+  btn.classList.add('bg-gray-400'); // Optional: You can change the background to gray to show it's disabled
+  btn.classList.remove('bg-blue-800'); // Remove the active button color
+ 
+
+  const countElement = document.getElementById('btn-complet');
+  let currentCount = parseInt(countElement.innerText); // Get the current number
+  if (currentCount > 0) {
+    countElement.innerText = currentCount + 1; // Decrease by 1
+  }
+
+  // Decrease the number in <h5> by 1
+  const secondCountElement = document.getElementById('sceund-number');
+  
+  
+  let currentSecondCount = parseInt(secondCountElement.innerText); // Get the current number
+  if (currentSecondCount > 0) {
+    secondCountElement.innerText = currentSecondCount - 1; // Decrease by 1
+  }
+  const historyElement = document.getElementById('history');
+  const historyMessage = `bsbyd`;
+  historyElement.innerHTML = historyMessage;
+
+   document.getElementById('history').style.backgroundColor = 'white'
+
+
+}
+
+
+
+
+function disablebuttonn() {
+  alert("Button clicked!");
+  const btn = document.getElementById('btn-completed-5');
+  btn.disabled = true;
+  btn.classList.add('bg-gray-400'); // Optional: You can change the background to gray to show it's disabled
+  btn.classList.remove('bg-blue-800'); // Remove the active button color
+ 
+
+  const countElement = document.getElementById('btn-complet');
+  let currentCount = parseInt(countElement.innerText); // Get the current number
+  if (currentCount > 0) {
+    countElement.innerText = currentCount + 1; // Decrease by 1
+  }
+
+  // Decrease the number in <h5> by 1
+  const secondCountElement = document.getElementById('sceund-number');
+  
+  
+  let currentSecondCount = parseInt(secondCountElement.innerText); // Get the current number
+  if (currentSecondCount > 0) {
+    secondCountElement.innerText = currentSecondCount - 1; // Decrease by 1
+  }
+  const historyElement = document.getElementById('history');
+  const historyMessage = `bsbyd`;
+  historyElement.innerHTML = historyMessage;
+
+   document.getElementById('history').style.backgroundColor = 'white'
+
+
+}
+
+
+
+
+
+function disableBtn() {
+  alert("Button clicked!");
+  const btn = document.getElementById('btn-completed-6');
+  btn.disabled = true;
+  btn.classList.add('bg-gray-400'); // Optional: You can change the background to gray to show it's disabled
+  btn.classList.remove('bg-blue-800'); // Remove the active button color
+ 
+
+  const countElement = document.getElementById('btn-complet');
+  let currentCount = parseInt(countElement.innerText); // Get the current number
+  if (currentCount > 0) {
+    countElement.innerText = currentCount + 1; // Decrease by 1
+  }
+
+  // Decrease the number in <h5> by 1
+  const secondCountElement = document.getElementById('sceund-number');
+  
+  
+  let currentSecondCount = parseInt(secondCountElement.innerText); // Get the current number
+  if (currentSecondCount > 0) {
+    secondCountElement.innerText = currentSecondCount - 1; // Decrease by 1
+  }
+  const historyElement = document.getElementById('history');
+  const historyMessage = `bsbyd`;
+  historyElement.innerHTML = historyMessage;
+
+   document.getElementById('history').style.backgroundColor = 'white'
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById('btn-comple').addEventListener('click', function(){
+  const historyElement = document.getElementById('history');
+  historyElement.innerText = '';
+})
